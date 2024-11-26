612bf08d420fe771998ba0c1b1408fffb55a76ac
sani73079 <83466179+sani73079@users.noreply.github.com>
2024-11-26 16:00:50 +0530

    Create If else.py

---
+++ If else.py
@@ -0,0 +1,13 @@
+greeting = "good night"
+if  greeting == "night": #condition matches second line
+    print("condition matches")
+    print("second line")
+else:
+    print("condtion do not match") #condtion do not match
+
+
+a=0
+if a>1:
+    print("condition match")
+else:
+    print("does not match")#does not match
