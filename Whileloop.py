e5beb9e63a9e7f07339dc547205cdd49b6311193
sani73079 <83466179+sani73079@users.noreply.github.com>
2024-11-26 16:03:14 +0530

    Create Whileloop.py

---
+++ Whileloop.py
@@ -0,0 +1,30 @@
+it = 5
+while it>1:
+    if it!=3: #if 3 is equal to 3 then it will skip the number
+        print(it)
+    it= it-1   #5432 while loop execution is done
+print("while loop execution is done")
+print("*********************0")
+
+it =4
+while it>1:
+    if it == 3:
+        break   # if it value 3 come and equal to 3 then code will brek and stop the execution there only.
+    print(it)
+    it = it-1
+print("while loop execution is done")
+
+
+print("***********************")
+
+it =11
+while it>1:
+    if it == 9:
+        it = it-1
+        continue   # if it value 3 come and equal to 3 then code will brek and stop the execution there only.
+    if it == 3:
+        break
+    print(it)
+    it = it-1
+print("while loop execution is done")
+
